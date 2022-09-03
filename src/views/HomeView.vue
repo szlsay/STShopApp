c4896eb4350a794b04d2f004f67e12a3417f3697
小沈曰 <409178030@qq.com>
2022-09-03 19:05:53 +0800

    search

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="home">
-    <van-search v-model="searchVal" disable placeholder="请输入搜索关键词" />
+    <van-search
+      v-model="searchVal"
+      shape="round"
+      placeholder="请输入搜索关键词"
+      disabled
+      @click="$router.push('/home/searchPopup')"
+    />
     <van-swipe class="my-swipe" :autoplay="3000" indicator-color="red">
-        <van-swipe-item v-for="item in banner" :key="item.id">
-           <img :src="item.image_url" alt="" width="100%">
-        </van-swipe-item>
+      <van-swipe-item v-for="item in banner" :key="item.id">
+        <img :src="item.image_url" alt="" width="100%" />
+      </van-swipe-item>
     </van-swipe>
+    <transition name="van-slide-right">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -20,14 +29,50 @@ export default {
     }
   },
   created() {
-    GetHomeLists().then((res) => {
-      const { banner } = res.data
-      this.banner = banner
-      console.log(this.banner)
-    }).catch(err => {
-      console.log('Error')
-      console.log(err)
-    })
+    GetHomeLists()
+      .then((res) => {
+        const { banner } = res.data
+        this.banner = banner
+        console.log(this.banner)
+      })
+      .catch((err) => {
+        console.log('Error')
+        console.log(err)
+      })
   }
 }
 </script>
+<style lang="less">
+// .slide-enter-from {
+//   /*进场初始效果*/
+//   right: -100%;
+// }
+// .slide-enter-active {
+//   transition: all 3s;
+// }
+// .slide-enter-to {
+//   /*进场最终效果*/
+//   right: 0;
+// }
+// .slide-enter-from,
+// .slide-leave-to {
+//   right: -100%;
+// }
+// .slide-enter-active,
+// .slide-leave-active {
+//   transition:right 3s;
+// }
+// .slide-enter-to,
+// .slide-leave-from {
+//   right: 0;
+// }
+// .fade-enter-active,
+// .fade-leave-active {
+//   transition: opacity 3s ease;
+// }
+
+// .fade-enter-from,
+// .fade-leave-to {
+//   opacity: 0;
+// }
+</style>
---
+++ src/views/SearchPopup.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="popup">
+    <van-search
+      v-model="searchVal"
+      show-action
+      :placeholder="placeholderVal"
+      @search="onSearch"
+      @cancel="onCancel"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchVal: '',
+      placeholderVal: ''
+    }
+  },
+  methods: {
+    onSearch(val) {
+      console.log('按了回车')
+    },
+    onCancel() {
+      // 点击了取消
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.popup {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  // right: 0;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
