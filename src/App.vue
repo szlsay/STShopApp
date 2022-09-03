c60b07d8e194ecdac5ddc4340cfc46aae234203a
小沈曰 <409178030@qq.com>
2022-09-03 18:08:21 +0800

    style

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,33 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
+    <span>Hello</span>
+    <div>Hello</div>
     <router-view/>
   </div>
 </template>
 
-<style>
-</style>
+<style lang="less">
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    min-height: 100%;
+    background-color: #efefef;
+    font-size: .14rem;
+  }
+  html,body{
+    height: 100%;
+  }
+  html{
+  font-size: 100px;
+}
+p{
+  width: 1rem;
+  height: 1rem;
+  background-color: red;
+}
+html{
+  font-size: 100px!important;
+}
+  </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,17 @@
     <span>Welcome to Your Vue.js App</span>
     <p>Home组件测试vant组件</p>
     <van-button type="primary">主要按钮</van-button>
+    <van-search v-model="searchVal" disable placeholder="请输入搜索关键词" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  data() {
+    return {
+      searchVal: ''
+    }
+  }
 }
 </script>
